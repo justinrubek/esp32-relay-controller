0cab778e0cf069adad00bb0c412c32ff5359b114
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2025-06-19 14:04:17 -0500

    feat: initialize rust project

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    bomper = {
+      url = "github:justinrubek/bomper";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-filter.url = "github:numtide/nix-filter";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux"];
+      imports = [
+        ./flake-parts/cargo.nix
+        ./flake-parts/ci.nix
+        ./flake-parts/formatting.nix
+        ./flake-parts/pre-commit.nix
+        ./flake-parts/rust-toolchain.nix
+        ./flake-parts/shells.nix
+      ];
+    };
+}
